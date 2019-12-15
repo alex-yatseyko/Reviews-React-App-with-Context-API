9bb4a66727acbee5582b9f0243580e0a040eabe5
Alex <alex.yatseiko@gmail.com>
2019-12-15 03:05:54 +0200

    add-review-added

--- src/components/Reviews.js
+++ src/components/Reviews.js
@@ -8,9 +8,10 @@ export const Reviews = () => {
     const reviews = useContext(ReviewsContext)
 
     return (
-        <div className="container">
+        <div className="container text-center">
             <h1 className="text-center mt-5">Welcome to Reviews</h1>
             <ReviewsInput />
+            <button type="button" className="btn btn-outline-primary btn-lg mt-5 mb-5" data-toggle="modal" data-target="#exampleModal">Add Review</button>
             <div className="row">
                 {reviews.map(r => <div className="col-md-4 mb-4" key={r.id}>
                 <ReviewItem 
--- src/components/ReviewsInput.js
+++ src/components/ReviewsInput.js
@@ -5,11 +5,46 @@ import { ReviewsContext } from '../inc/ReviewsContext'
 export const ReviewsInput = () => {
     const reviews = useContext(ReviewsContext)
 
-    const [input, setInput] = useState('')
+    const [input, setInput] = useState({
+        title: '',
+        text: '',
+        author: '',
+        id: Math.random()
+    })
 
-    const onChange = e => {
-        setInput(e.target.value)
+    const onChangeTitle = e => {
+        setInput({
+            title: e.target.value,
+            text: input.text,
+            author: input.author,
+            id: input.id
+        })
+        console.log(input)
     }
+
+    const onChangeText = e => {
+        setInput({
+            title: input.title,
+            text: e.target.value,
+            author: input.author,
+            id: input.id
+        })
+        console.log(input)
+    }
+
+    const onChangeAuthor = e => {
+        setInput({
+            title: input.title,
+            text: input.text,
+            author: e.target.value,
+            id: input.id
+        })
+        console.log(input)
+    }
+
+    // const onChange = e => {
+    //     setInput(e.target.value)
+    // }
 
     const onAdd = (value) => {
         reviews.push(value)
@@ -25,12 +60,33 @@ export const ReviewsInput = () => {
 
     return (
         <div>
-            <form className="card mb-3 mt-3 pt-3 pb-3 pl-3 pr-3">
-                <div className="form-group">
-                    <input placeholder="Review title" onChange={onChange} value={input} type="text" style={{width: '100%', height: '3rem', padding: '20px', fontSize: '26px'}}/>
+            <div className="modal fade mt-5" id="exampleModal"  role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div className="modal-dialog" role="document">
+                <div className="modal-content">
+                <div className="modal-header">
+                    <h5 className="modal-title" id="exampleModalLabel">Add Review</h5>
+                    <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                    </button>
                 </div>
-                <button onClick={onSubmit} type="submit" className="btn btn-primary">Submit</button>
-            </form>
+                <form className="mb-3 mt-3 pt-3 pb-3 pl-3 pr-3">
+                <div className="modal-body">
+                    <div className="form-group">
+                        <input placeholder="Review title" onChange={onChangeTitle} value={input.title} type="text" className="reviewInput"/>
+                        <input placeholder="Review Text" onChange={onChangeText} value={input.text } type="text" className="reviewInput mt-3"/>
+                        <input placeholder="Review Author" onChange={onChangeAuthor} value={input.author } type="text" className="reviewInput mt-3"/>
+                    </div>
+                </div>
+                <div className="modal-footer text-center" style={{justifyContent: 'center'}}>
+                    <button onClick={onSubmit} type="submit" className="btn btn-primary" data-dismiss="modal">Submit</button>
+                </div>
+
+                    
+                </form>
+
+                </div>
+            </div>
+            </div>
         </div>
     )
 }
